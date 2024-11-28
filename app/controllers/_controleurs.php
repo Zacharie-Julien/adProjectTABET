fbeef09861cd365b179849690a3ff2bee4dcbfb9
lucasjgd <163435767+lucasjgd@users.noreply.github.com>
2024-11-28 17:09:59 +0100

    Update _controleurs.php

--- app/controllers/_controleurs.php
+++ app/controllers/_controleurs.php
@@ -1,7 +1,8 @@
 <?php
 //require_once './accueilControleur.php';
 require_once 'inscrireControleur.php';
-//require_once 'connexionControleur.php';
+require_once 'connexionControleur.php';
+require_once 'creationControleur.php';
 //require_once 'utilisateurControleur.php';
 //require_once 'typeControleur.php';
 //require_once 'produitControleur.php';
