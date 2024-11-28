59fcd540a08d9a31ee1de0509ef0f7283327cfba
Olympique13 <153452052+Olympique13@users.noreply.github.com>
2024-11-28 17:17:23 +0100

    Update _controleurs.php

--- app/controllers/_controleurs.php
+++ app/controllers/_controleurs.php
@@ -3,6 +3,7 @@
 require_once 'inscrireControleur.php';
 require_once 'connexionControleur.php';
 require_once 'creationControleur.php';
+require_once 'gestionUserControleur.php';
 //require_once 'utilisateurControleur.php';
 //require_once 'typeControleur.php';
 //require_once 'produitControleur.php';
